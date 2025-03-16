8964297e10085fa139bbac71422ca3aa744c1a0c
Anastasiya Shevrina <shevrina@inbox.ru>
2025-03-16 19:49:22 +0700

    add functions for card mask

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from .src.masks import get_mask_card_number
 from .src.masks import get_mask_account
+from .src.widget import mask_account_card
+from .src.widget import get_date
 
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+# print(get_mask_card_number(7000792289606361))
+# print(get_mask_account(73654108430135874305))
+
+print(mask_account_card('Visa Platinum 7000792289606361'))
+print(mask_account_card('Счет 73654108430135874305'))
+
+
