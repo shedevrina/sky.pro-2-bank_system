4dd5ba5b38e6b167e0eee660fac146693574283c
Anastasiya Shevrina <shevrina@inbox.ru>
2025-03-16 20:33:14 +0700

    debag virtual src.widget.py and maim.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from .src.masks import get_mask_card_number
-from .src.masks import get_mask_account
-from .src.widget import mask_account_card
-from .src.widget import get_date
-
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 print(get_mask_card_number(7000792289606361))
 print(get_mask_account(73654108430135874305))
 
-print(mask_account_card('Visa Platinum 7000792289606361'))
-print(mask_account_card('Счет 73654108430135874305'))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
 
-print(get_date('2024-03-11T02:26:18.671407'))
-
-
-
+print(get_date("2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,19 @@
 def mask_account_card(account: str) -> str:
-    '''Тип и номер карты или счета маскируется. Для карты и счёта разные маски.'''
-    if 'Счет' in account:
-        return (f'{account[:4]} **{account[-4:]}')
+    """Тип и номер карты или счета маскируется. Для карты и счёта разные маски."""
+    if "Счет" in account:
+        return f"{account[:4]} **{account[-4:]}"
     else:
-        account_set = account.split(' ')
-        account_set_number_mask = f'{account_set[-1][:4]} {account_set[-1][5:7]}** **** {account_set[-1][-4:]}'
+        account_set = account.split(" ")
+        account_set_number_mask = f"{account_set[-1][:4]} {account_set[-1][5:7]}** **** {account_set[-1][-4:]}"
         account_set[-1] = account_set_number_mask
-        return ' '.join(account_set)
+        return " ".join(account_set)
+
 
 def get_date(date_format: str) -> str:
-    '''Преобразования формата даты из "2024-03-11T02:26:18.671407" в "ДД.ММ.ГГГГ" ("11.03.2024")'''
-    date_format_set = date_format.split('T')
-    date_format_set_day = date_format_set[0].split('-')
-    return f'{date_format_set_day[-1]}.{date_format_set_day[1]}.{date_format_set_day[0]}'
+    """Преобразования формата даты из "2024-03-11T02:26:18.671407" в "ДД.ММ.ГГГГ" ("11.03.2024")"""
+    date_format_set = date_format.split("T")
+    date_format_set_day = date_format_set[0].split("-")
+    return f"{date_format_set_day[-1]}.{date_format_set_day[1]}.{date_format_set_day[0]}"
+
+
+print("Hi")
