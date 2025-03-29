c034052a6a565ece36bf157ef6c3fc371ae9e93b
Anastasiya Shevrina <shevrina@inbox.ru>
2025-03-29 20:09:04 +0700

    Editing for PEP8

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,20 @@
-def filter_by_state(transactions: list[{}], state = 'EXECUTED') -> list[{}]:
-    '''Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
+def filter_by_state(transactions: list[{}], state="EXECUTED") -> list[{}]:
+    """Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует
-    указанному значению.'''
+    указанному значению."""
     new_dict = []
 
     for transaction in transactions:
-        if transaction['state'] == state:
+        if transaction["state"] == state:
             new_dict.append(transaction)
         else:
             continue
 
     return new_dict
 
+
 def sort_by_date(transactions: list[{}], reverse_status=True) -> list[{}]:
-    '''Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
-    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date).'''
-    sorted_date = sorted(transactions, key=lambda x: x['date'], reverse = reverse_status)
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date)."""
+    sorted_date = sorted(transactions, key=lambda x: x["date"], reverse=reverse_status)
     return sorted_date
