e89f2dd5e04645d5f61f83cfd11453105c742fa8
Anastasiya Shevrina <shevrina@inbox.ru>
2025-04-10 18:54:17 +0700

    Code revision after adding tests

--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,50 @@
+from datetime import datetime
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(account: str) -> str:
-    """Тип и номер карты или счета маскируется. Для карты и счёта разные маски."""
-    if "Счет" in account:
-        return f"{account[:4]} **{account[-4:]}"
+    """Для title. Тип и номер карты или счета маскируется. Для карты и счёта разные маски."""
+    if not isinstance(account, str):
+        raise TypeError("Не верный тип данных")
     else:
-        account_set = account.split(" ")
-        account_set_number_mask = f"{account_set[-1][:4]} {account_set[-1][5:7]}** **** {account_set[-1][-4:]}"
-        account_set[-1] = account_set_number_mask
-        return " ".join(account_set)
+
+        if account.split(" "):
+            account_set = account.split(" ")
+
+            if len(account_set) >= 2:
+                print(account_set)
+                number = int(account_set[-1])
+
+                if "Счет" in account_set:
+                    mask_account = get_mask_account(int(number))
+                    return f"Счет {mask_account}"
+
+                elif "Visa" in account_set or "Maestro" in account_set:
+                    mask_number = get_mask_card_number(int(number))
+                    massage = " ".join(account_set[:-1])
+                    return f"{massage} {mask_number}"
+                else:
+                    raise ValueError("Ошибка значения данных")
+        else:
+            raise ValueError("Ошибка значения данных")
 
 
 def get_date(date_format: str) -> str:
     """Преобразования формата даты из "2024-03-11T02:26:18.671407" в "ДД.ММ.ГГГГ" ("11.03.2024")"""
-    date_format_set = date_format.split("T")
-    date_format_set_day = date_format_set[0].split("-")
-    return f"{date_format_set_day[-1]}.{date_format_set_day[1]}.{date_format_set_day[0]}"
+    if type(date_format) != str:
+        raise TypeError("Не верный тип данных")
+    else:
 
+        if len(date_format) >= 11 and "T" in date_format:
+            data_day = date_format.split("T")
+            list_data = list(data_day[0].split("-"))
+            for i in list_data:
+                if int(i) > 0:
+                    continue
+                else:
+                    raise ValueError("Ошибка даты в ISO")
 
-print("Hi")
+            date_object = datetime.strptime(data_day[0], "%Y-%m-%d").date()
+            return f"{date_object.day}.{date_object.month}.{date_object.year}"
+        else:
+            raise ValueError("Ошибка даты в ISO")
