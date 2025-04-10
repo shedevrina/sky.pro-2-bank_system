4fdefe7afcb192ba21f7a83b09c9574f86b53ae7
Anastasiya Shevrina <shevrina@inbox.ru>
2025-04-10 18:52:55 +0700

    Rewrite argument for print

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
-print(get_mask_card_number(7000792289606361))
+print(get_mask_card_number(1000100010001))
 print(get_mask_account(73654108430135874305))
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
 
 print(get_date("2024-03-11T02:26:18.671407"))
+print(get_date("1998-01-12T"))
 
 
-# homework_10_1
+
+#homework_10_1
 print(
     filter_by_state(
         [
