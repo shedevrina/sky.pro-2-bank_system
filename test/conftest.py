0d449e9094d5aeb2e4f1f62ee9eca6c4fce292c5
Anastasiya Shevrina <shevrina@inbox.ru>
2025-04-16 19:58:02 +0700

    Add new @pytest.fixture for modul "generators.py"

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,6 @@
 import pytest
 
 # Тестирования модуля masks.py
-
-
 @pytest.fixture
 def coll_number():
     """Номер карты пользователя"""
@@ -32,8 +30,6 @@ def coll_data_negative_value():
 
 
 # Тестирования модуля processing.py
-
-
 @pytest.fixture
 def coll_list_dict():
     return [
@@ -54,3 +50,37 @@ def coll_negative_list_data():
         {"id": 41428829, "state": "EXECUTED", "date": "2019-00-0T18:35:29.512364"},
         {"id": 939719570, "state": "CANCELED", "date": ""},
     ]
+
+
+# Тестирования модуля generators.py
+@pytest.fixture
+def coll_transactions():
+    return [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "RUB", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 142264269,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+]
